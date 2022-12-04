9d1cffd2023cb256263286cbf60b9ac7427a1426
Thomas Steck <thomas.steck@ingka.ikea.com>
2022-12-04 08:24:11 +0100

    typo fix

--- 4.py
+++ 4.py
@@ -8,10 +8,10 @@ for r in d:
     if r == "":
         continue
     a, b = [(int(v[0]), int(v[1])) for v in [v.split("-") for v in r.split(",")]]
-    if (a[0] >= b[0] and a[1] <= b[1]) or (b[0] >= a[0] and b[1] <= a[1]):
+    if a[0] >= b[0] and a[1] <= b[1] or b[0] >= a[0] and b[1] <= a[1]:
         total1 += 1
     if a[0] >= b[0] and a[1] <= b[1] or b[0] <= a[1] and b[1] >= a[0]:
         total2 += 1
 
 print("total 1: ", total1)
-print("total 1: ", total2)
+print("total 2: ", total2)
